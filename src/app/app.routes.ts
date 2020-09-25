e2e1b922c191696e3b5fa216b189597b3e36ddef
DivianaPino <Divianap96@gmail.com>
2020-09-25 19:31:23 -0400

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,14 @@
 import {RouterModule , Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 
 
 
 const appRoutes: Routes= [
-    {
-        path: '' ,
-        component: PagesComponent,
-        children: [ //rutas hijas 
-            {path: 'dashboard' , component: DashboardComponent}, 
-            {path: 'progress' , component: ProgressComponent},
-            {path: 'graficas1' , component: Graficas1Component},
-            {path: '' , redirectTo: '/dashboard', pathMatch: 'full'},  //Cualquier ruta vacia se va redireccionar a la ruta '/dashboard'
-        ]
-    },
-    
+        
     {path: 'login' , component: LoginComponent},
     {path: 'register' , component: RegisterComponent},
     {path: '**' , component: NopagefoundComponent} // Cualquier otra ruta que no este definida, se mostrara el componente 'Nopagefound'
